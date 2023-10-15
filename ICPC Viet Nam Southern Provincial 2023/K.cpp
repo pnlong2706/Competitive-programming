e404097e576eac4a9aa4f016372585e82bcc348c
Pham Ngoc Long <77040324+pnlong2706@users.noreply.github.com>
2023-10-15 20:20:21 +0700

    Create K.cpp

---
+++ ICPC Viet Nam Southern Provincial 2023/K.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll          long long
+#define ull         unsigned long long
+#define for_(n)     for(ll i=0; i<n; i++)
+#define for__(a,b)  for(ll i=a; i<b; i++)
+#define _for(i,a,b) for(ll i=a; i<b; i++)
+#define mp          make_pair
+#define fi          first
+#define se          second
+#define pb          push_back
+#define pii         pair<int, int>
+#define pll         pair<long long, long long>
+#define el          "\n"
+#define debug(x)    cerr << "[debug] " << #x << " : " << x << endl;
+
+const long long MOD=1000000007;
+
+template<class T, class P>
+ostream& operator<<(ostream& os, const pair<T,P> &a) { os << "{" << a.first << ";" << a.second << "}"; return os; }
+template<class T>
+ostream& operator<<(ostream& os, const vector<T> &a) { ; for(auto it: a) os << it << " "; ; return os; }
+template<class T>
+ostream& operator<<(ostream& os, const deque<T> &a) { ; for(auto it: a) os << it << " "; ; return os; }
+template<class T>
+ostream& operator<<(ostream& os, const set<T> &a) { ; for(auto it: a) os << it << " "; ; return os; }
+template<class T>
+ostream& operator<<(ostream& os, const multiset<T> &a) { ; for(auto it: a) os << it << " "; ; return os; }
+
+struct lessSet { bool operator() (const long long &a, const long long &b) { return a>b;} };
+
+ll gcd(ll a, ll b) { return b==0? a : gcd(b,a%b); }
+ll lcm(ll a, ll b) { return a/(gcd(a,b))*b; }
+
+ll pow_mod(ll x, ll y, ll mod) { //mod<3.10^9
+    ll ans=1;
+    while(y>0) {
+        if(y%2==1) ans=ans*x%mod;
+        y=y>>1;
+        x=x*x%mod;
+    }
+    return ans%mod;
+}
+
+const int BS=10000;
+
+void solve() {
+    int n, k, ml=1e9;
+    cin >> n;
+    vector<int> a(n), mr(n,INT_MAX);
+    for_(n) cin >> a[i];
+    mr[n-1]=a[n-1]+1;
+    for(int i=n-2; i>=0; i--) mr[i]=min( mr[i+1]+1,a[i]+1 );
+
+    int ans=1;
+    for_(n-1) {
+        int ta, v=a[i];
+
+        ta=min( {v,ml,mr[i+1],(int)i+1,n-(int)i} );
+        ans=max(ans,ta);
+        ml=min(ml+1,v+1);
+    }
+
+    cout << ans << el;
+
+}
+
+
+int main() {
+    //clock_t begin=clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("INP.inp","r",stdin);
+    //freopen( "OUT.out", "w", stdout );
+    int t=1;
+    //cin >> t;
+    while(t--)
+        solve();
+    //cout << "TIME : " << (double)(clock()-begin)/CLOCKS_PER_SEC << "s.";
+    return 0;
+}
