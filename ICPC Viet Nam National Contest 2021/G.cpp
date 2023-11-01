a51ea90247a19753ae5e48e4c2d064b2913e0b78
Pham Ngoc Long <77040324+pnlong2706@users.noreply.github.com>
2023-11-01 18:42:20 +0700

    Create G.cpp

---
+++ ICPC Viet Nam National Contest 2021/G.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define ull         unsigned long long
+#define for_(n)     for(int i=0; i<n; i++)
+#define for__(a,b)  for(int i=a; i<b; i++)
+#define mp          make_pair
+#define fi          first
+#define se          second
+#define pb          push_back
+#define pii         pair<int, int>
+#define pll         pair<long long, long long>
+#define el          "\n"
+
+const long long MOD=998244353;
+
+
+template<class T, class P>
+ostream& operator<<(ostream& os, const pair<T,P> &a) { os << "{" << a.first << ";" << a.second << "}"; return os; }
+template<class T>
+ostream& operator<<(ostream& os, const vector<T> &a) { ; for(auto it: a) os << it << " "; ; return os; }
+template<class T>
+ostream& operator<<(ostream& os, const deque<T> &a) { ; for(auto it: a) os << it << " "; ; return os; }
+template<class T>
+ostream& operator<<(ostream& os, const multiset<T> &a) { ; for(auto it: a) os << it << " "; ; return os; }
+struct lessSet { bool operator() (const long long &a, const long long &b) { return a>b;} };
+
+
+ll gcd(ll a, ll b) { return b==0? a : gcd(b,a%b); }
+ll lcm(ll a, ll b) { return a/(gcd(a,b))*b; }
+
+ll pow_mod(ll x, ll y, ll mod) { //mod<3.10^9
+    ll ans=1;
+    while(y>0) {
+        if(y%2==1) ans=ans*x%mod;
+        y=y>>1;
+        x=x*x%mod;
+    }
+    return ans%mod;
+}
+
+
+
+void solve() {
+    int n, k, ans1=0, ans2=0;
+    cin >> n >> k;
+    vector<int> a(n);
+    for_(n) cin >> a[i];
+
+    sort(a.begin(),a.end());
+    for_(k) {
+        if(a[i]!=1) ans1+=a[i];
+        if(a[n-i-1]!=1) ans2+=a[n-i-1];
+    }
+
+    cout << ans1 << " " << ans2;
+
+}
+
+int main() {
+    //clock_t begin=clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("INP.inp","r",stdin);
+    //int t;
+    //cin >> t;
+    //while(t--)
+        solve();
+
+    //cout << "TIME : " << (double)(clock()-begin)/CLOCKS_PER_SEC << "s.";
+    return 0;
+}
+
